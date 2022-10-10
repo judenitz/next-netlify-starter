24a13ab0de5ff50297d245f0982ccf289c021ccf
judenitz <115475533+judenitz@users.noreply.github.com>
2022-10-10 15:28:04 +0200

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -3,9 +3,7 @@ import styles from './Footer.module.css'
 export default function Footer() {
   return (
     <>
-      <footer className={styles.footer}>
-       
-      </footer>
+
     </>
   )
 }
