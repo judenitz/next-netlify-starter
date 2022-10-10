ca5a375f6826779c0c56e61699ee678f5f09fc45
judenitz <115475533+judenitz@users.noreply.github.com>
2022-10-10 15:15:32 +0200

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        Made with <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} /> for you
+        kontakt: michal [at] knajp.pl / 501 499 771
       </footer>
     </>
   )
