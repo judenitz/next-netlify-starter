9f38c8a05b55e3d6796e7f1007cadcd9d3ea8de2
judenitz <115475533+judenitz@users.noreply.github.com>
2022-10-10 16:08:14 +0200

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
     <footer className={styles.footer}>
-       
+       kontakt: michal [at] knajp.pl&nbsp;/&nbsp;501 499 771
       </footer>
     </>
   )
